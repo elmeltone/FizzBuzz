cffcc4e387b545c84d26f9a59dd143e60a625fa0
MICHAEL J MELTON <meltonm83@gmail.com>
2016-02-04 13:13:07 -0800

    print array

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 
+var fbNumbers = [];
+
+for(var i=1; i !=101; ++i) fbNumbers.push(i);
+
+document.write(fbNumbers);
+
+/*for (i>=1; i<=100;) {
+  if
+}*/
 
 
 
 
 
 
-};
+});
