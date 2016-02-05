caebd39f63794badc18240e478d6c4af08b75334
MICHAEL J MELTON <meltonm83@gmail.com>
2016-02-04 21:06:17 -0800

    lines

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
 
 var fbNumbers = [];
-var fizz = i%3;
-var buzz = i%5;
-
-for(var i=1; i !=101; ++i) fbNumbers.push(i);
-
-/*for( s in fbNumbers) {
-  if (buzz=0) console.log;
-}*/
 
 
-/*for(var i=1; fbNumbers[i]; ++i) {
-  if (fizz=0) fbNumbers.replace("Fizz");
-  if (buzz=0) fbNumbers.replace("Buzz");
-  //if (fizz=0 && buzz=0) fbNumbers.splice("FizzBuzz");
-  };*/
+for(var i=0; i !=101; i++) {
+  fbNumbers.push(i);
+};
+
+for(var i=0; i !=101; i++) {
+  if (fbNumbers[i]%3 == 0)
+    document.write("Fizz");
+  else if (fbNumbers[i]%5 == 0)
+    document.write("Buzz");
+  else if (fbNumbers[i]%3 == 0 && fbNumbers[i]%5 == 0)
+    document.write("FizzBuzz");
+  else document.write
+    (fbNumbers[i]);
+
+  document.write("<br>");
+  };
 
 
-
-//for (i%3==0) return "buzz";
-
-document.write(fbNumbers);
+//document.write(fbNumbers);
 
 
 
