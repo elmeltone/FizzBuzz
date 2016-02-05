78201a7ebb3d424d269aa94259cfbedfac73ddd1
MICHAEL J MELTON <meltonm83@gmail.com>
2016-02-05 11:04:30 -0800

    all three

--- js/app.js
+++ js/app.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
 var fbNumbers = [];
 
 
-for(var i=0; i !=101; i++) {
+for(var i=1; i !=101; i++) {
   fbNumbers.push(i);
 };
 
 for(var i=0; i !=101; i++) {
-  if (fbNumbers[i]%3 == 0)
-    document.write("Fizz");
-  else if (fbNumbers[i]%5 == 0)
-    document.write("Buzz");
-  else if (fbNumbers[i]%3 == 0 && fbNumbers[i]%5 == 0)
+  if (fbNumbers[i]%3 == 0 && fbNumbers[i]%5 == 0)
     document.write("FizzBuzz");
+  else if (fbNumbers[i]%3 == 0 && fbNumbers[i]%5 != 0)
+    document.write("Fizz");
+  else if (fbNumbers[i]%3 != 0 && fbNumbers[i]%5 == 0)
+    document.write("Buzz");
   else document.write
-    (fbNumbers[i]);
+    (fbNumbers[i]) return "";
 
   document.write("<br>");
   };
