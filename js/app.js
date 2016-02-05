efa2553e018de0367409116c87633f19737aefa8
MICHAEL J MELTON <meltonm83@gmail.com>
2016-02-04 17:22:15 -0800

    document write

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
 
 var fbNumbers = [];
+var fizz = i%3;
+var buzz = i%5;
 
 for(var i=1; i !=101; ++i) fbNumbers.push(i);
 
+if (fizz=0) fbNumbers.replace("Fizz");
+if (buzz=0) fbNumbers.replace("Buzz");
+if (fizz=0 && buzz=0) fbNumbers.replace("FizzBuzz");
+
+
+
+//for (i%3==0) return "buzz";
+
 document.write(fbNumbers);
 
-/*for (i>=1; i<=100;) {
-  if
-}*/
 
 
 
