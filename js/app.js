a00cce09260d67e683ad6d24e4ac91da2e3355b6
MICHAEL J MELTON <meltonm83@gmail.com>
2016-02-04 20:18:39 -0800

    progress

--- js/app.js
+++ js/app.js
@@ -4,11 +4,16 @@ var fbNumbers = [];
 var fizz = i%3;
 var buzz = i%5;
 
-for(var i=1; i !=101; ++i) fbNumbers.push(i);
+for( s in fbNumbers) {
+  if (buzz=0) console.log;
+}
 
-if (fizz=0) fbNumbers.replace("Fizz");
-if (buzz=0) fbNumbers.replace("Buzz");
-if (fizz=0 && buzz=0) fbNumbers.replace("FizzBuzz");
+
+/*for(var i=1; fbNumbers[i]; ++i) {
+  if (fizz=0) fbNumbers.replace("Fizz");
+  if (buzz=0) fbNumbers.replace("Buzz");
+  //if (fizz=0 && buzz=0) fbNumbers.splice("FizzBuzz");
+  };*/
 
 
 
