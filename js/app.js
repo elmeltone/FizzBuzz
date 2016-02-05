3352b0ff843ceed8252eb853c898cb96b6e3518a
MICHAEL J MELTON <meltonm83@gmail.com>
2016-02-05 11:18:52 -0800

    functional

--- js/app.js
+++ js/app.js
@@ -8,22 +8,18 @@ for(var i=1; i !=101; i++) {
 };
 
 for(var i=0; i !=101; i++) {
+
+
   if (fbNumbers[i]%3 == 0 && fbNumbers[i]%5 == 0)
     document.write("FizzBuzz");
   else if (fbNumbers[i]%3 == 0 && fbNumbers[i]%5 != 0)
     document.write("Fizz");
   else if (fbNumbers[i]%3 != 0 && fbNumbers[i]%5 == 0)
     document.write("Buzz");
-  else document.write
-    (fbNumbers[i]) return "";
-
+  else document.write(fbNumbers[i]);
   document.write("<br>");
+
   };
-
-
-//document.write(fbNumbers);
-
-
 
 
 
